9ea69226faa24f3617d75782445946e9b000870f
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-26 16:41:55 +0100

    feat: add array and imgs

---
+++ foods.js
@@ -0,0 +1,36 @@
+const noticeBoard = [
+    {
+        titolo: 'Ciambellone',
+        contenuto: 'Dessert',
+        immagine: 'imgs/foods/ciambellone.jpeg',
+        tags: ['#ciambellone', '#torta', '#dolce']
+    },
+
+    {
+        titolo: 'Cracker di barbabietola',
+        contenuto: 'Antipasto',
+        immagine: 'imgs/foods/cracker_barbabietola.jpeg',
+        tags: ['#cracker', '#barbabietola', '#salato']
+    },
+
+    {
+        titolo: 'Pane fritto dolce',
+        contenuto: 'Dessert',
+        immagine: 'imgs/foods/pane_fritto.jpeg',
+        tags: ['#pane', '#fritto', '#dolce']
+    },
+
+    {
+        titolo: 'Pasta alla barbabietola',
+        contenuto: 'Primo',
+        immagine: 'imgs/foods/pasta_barbabietola.jpeg',
+        tags: ['#pasta', '#barbabietola', '#salato']
+    },
+
+    {
+        titolo: 'Torta paesana',
+        contenuto: 'Dessert',
+        immagine: 'imgs/foods/torta_paesana.jpeg',
+        tags: ['#torta', '#paesana', '#dolce']
+    }
+]
