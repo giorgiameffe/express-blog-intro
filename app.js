b2e9410e04fb7969e390e05d0924829480b15c92
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-26 16:07:07 +0100

    feat: add first route

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ const app = express();
 
 // identificare la porta 
 const port = 3000;
+
+app.get('/', (req,res) => {
+    console.log('Ho chiamato la rotta principale');
+    res.send('Server del mio blog');
+})
+
+app.listen(port, ()=> {
+    console.log(`Sono un server attivo sulla porta ${port}`);
+})
