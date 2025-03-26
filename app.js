ef1588060e343a43c7cabcb1bec208c6a872de14
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-26 15:52:30 +0100

    feat: import express, identify port

---
+++ app.js
@@ -0,0 +1,8 @@
+// importare express
+const express = require('express');
+
+// salvare in una variabile l'invocazione della funzione express
+const app = express();
+
+// identificare la porta 
+const port = 3000;
